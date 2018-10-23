b20fe8939a778ad375af5e36fc0b93d61b5e105c
BACHAN <amitab.gwebs@gmail.com>
2018-10-23 13:22:29 +0530

    config update

--- deploy-live.sh
+++ deploy-live.sh
@@ -6,9 +6,9 @@ touch ./version.txt
 echo "$now" > ./version.txt
 
 echo "Sending files to server"
-rsync -arzP --delete --exclude=/\.git/ --exclude 'exclude-live-me.txt' --exclude-from 'exclude-live-me.txt'  ./ --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -e "ssh -p 7822" gnetserv@gnetserver.com:/home/gnethost.com/uckkr
+rsync -arzP --delete --exclude=/\.git/ --exclude 'exclude-live-me.txt' --exclude-from 'exclude-live-me.txt'  ./ --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -e "ssh -p 7822" gnetserv@gnetserver.com:/home/gnethost.com/uckkr/testing
 
 echo "Updating file permissions"
-ssh gnetserv@gnetserver.com -p 7822 'chown -R gnetserv:gnetserv  /home/gnetserv/gnethost.com/uckkr'
+ssh gnetserv@gnetserver.com -p 7822 'chown -R gnetserv:gnetserv  /home/gnetserv/gnethost.com/uckkr/testing'
 
 echo "Done"
