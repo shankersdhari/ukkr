f87aaff433ae3b5bace13ecd8460fb0b95a4ff7d
BACHAN <amitab.gwebs@gmail.com>
2018-10-23 13:13:15 +0530

    config update

---
+++ deploy-live.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Creating version.txt"
+now="$(date +'%Y.%m.%d')"
+touch ./version.txt
+echo "$now" > ./version.txt
+
+echo "Sending files to server"
+rsync -arzP --delete --exclude=/\.git/ --exclude 'exclude-live-me.txt' --exclude-from 'exclude-live-me.txt'  ./ --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -e "ssh -p 7822" gnetserv@gnetserver.com:/home/gnethost.com/uckkr
+
+echo "Updating file permissions"
+ssh gnetserv@gnetserver.com -p 7822 'chown -R gnetserv:gnetserv  /home/gnetserv/gnethost.com/uckkr'
+
+echo "Done"
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Creating version.txt"
+now="$(date +'%Y.%m.%d')"
+touch ./version.txt
+echo "$now" > ./version.txt
+
+echo "Sending files to server"
+rsync -arzP --delete --exclude=/\.git/ --exclude 'exclude-me.txt' --exclude-from 'exclude-me.txt'  ./ --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -e "ssh -p 7822" gnetserv@gnetserver.com:/home/gnetserv/gnethost.com/uckkr/testing
+
+echo "Updating file permissions"
+ssh gnetserv@gnetserver.com -p 7822 'chown -R gnetserv:gnetserv  /home/gnetserv/gnethost.com/uckkr/testing'
+
+echo "Done"
