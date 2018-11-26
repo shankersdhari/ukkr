2afc7b53db199efb548e9b1afe0bf99cdf06dac3
Gurpal <gurpal.gwebs@gmail.com>
2018-11-26 15:35:58 +0530

    404 page changes

--- _protected/frontend/views/site/error.php
+++ _protected/frontend/views/site/error.php
@@ -13,8 +13,8 @@ $this->context->layout = 'simple';
 	<div class="error-page">
 		<div class="error-img"><img src="<?= Yii::$app->params['baseurl'] ?>/images/error-img.jpg" alt="error-img" title="error-img"></div>
 		<div class="error-text">
-			<h4>Class is over ! Page is not found</h4>
-			<a href="<?= Yii::$app->params['baseurl'] ?>">Back to home</a>
+			<h4>Oops ! Page not found</h4>
+			<a href="https://uckkr.org/">Back to home</a>
 		</div>
 	</div>
 </div>
